37dc0e3438c0720c17fca221be4c705b3d0426a7
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-25 10:36:54 -0800

    add mapbox and leaflet map data

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+
+// Mapbox base map URL
+const accessToken = 'pk.eyJ1IjoianVsaWFqNjIxIiwiYSI6ImNrM2VxeTM3YTAxNGMzY253YjQwZzI4dzgifQ.kPd5-63cgQtFaBXWjc4IWQ'
+const tileUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
+const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+// Map settings
+const maxZoom = 19
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      zoom: 12,
+      mapCenter: [49.2827, -123.1207],
     }
   }
 
@@ -25,12 +35,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
-      </div>
+      // <div className="App">
+      //   <h1>{ this.state.message }</h1>
+      //   <button onClick={this.fetchData} >
+      //     Fetch Data
+      //   </button>        
+      // </div>
+      <Map
+      ref={m => { this.leafletMap = m }}
+      id="mapid"
+      center={this.state.mapCenter}
+      zoom={this.state.zoom}
+      maxZoom={maxZoom}
+      >
+      <TileLayer
+        attribution={attribution}
+        url={tileUrl}
+        id={'mapbox.light'}
+        accessToken={accessToken}
+      />
+      </Map>
     );
   }
 }
