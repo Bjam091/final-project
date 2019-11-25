c44bd1efb32091549e7060983f3a5038e6e7e37b
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-25 11:01:23 -0800

    remove default marker and add custom marker

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 
 // Mapbox base map URL
 const accessToken = 'pk.eyJ1IjoianVsaWFqNjIxIiwiYSI6ImNrM2VxeTM3YTAxNGMzY253YjQwZzI4dzgifQ.kPd5-63cgQtFaBXWjc4IWQ'
@@ -10,13 +13,27 @@ const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">Op
 // Map settings
 const maxZoom = 19
 
+// removes the default marker icon
+delete L.Icon.Default.prototype._getIconUrl;
+// attaches and styles a new marker icon
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+const customIcon = new L.Icon({
+  iconUrl: require('./assets/marker.png'),
+  iconSize: [25, 25]
+})
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       message: 'Click the button to load data!',
       zoom: 12,
-      mapCenter: [49.2827, -123.1207],
+      mapCenter: [49.2812, -123.1149],
     }
   }
 
@@ -33,6 +50,14 @@ class App extends Component {
     }) 
   }
 
+  // componentDidMount = () => {
+  //   fetch(`http://localhost:5000/trees`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       this.setState({trees: data.features})
+  //     })
+  // }
+
   render() {
     return (
       // <div className="App">
@@ -42,11 +67,11 @@ class App extends Component {
       //   </button>        
       // </div>
       <Map
-      ref={m => { this.leafletMap = m }}
-      id="mapid"
-      center={this.state.mapCenter}
-      zoom={this.state.zoom}
-      maxZoom={maxZoom}
+        ref={m => { this.leafletMap = m }}
+        id="mapid"
+        center={this.state.mapCenter}
+        zoom={this.state.zoom}
+        maxZoom={maxZoom}
       >
       <TileLayer
         attribution={attribution}
@@ -54,6 +79,28 @@ class App extends Component {
         id={'mapbox.light'}
         accessToken={accessToken}
       />
+      <MarkerClusterGroup
+            >
+          {/* {this.state.trees.map(tree => {
+            return (
+                <Marker
+                  key={`marker_${tree.properties.id}`}
+                  position={[tree.geometry.coordinates[1], tree.geometry.coordinates[0]]}
+                  icon={customIcon}
+                  style={{fontColor: '#edc4bc'}}
+                >
+                <Popup key={`pop_${tree.properties.id}`}>
+                  {tree.properties.common_name ? (<div>{`Common name: ${tree.properties.common_name}`}</div>) : null}
+                  {tree.properties.scientific_name ? (<div>{`Scientific name: ${tree.properties.scientific_name}`}</div>) : null}
+                  {tree.properties.fam_name ? (<div>{`Family: ${tree.properties.fam_name}`}</div>) : null}
+                  {tree.properties.genus_name ? (<div>{`Genus: ${tree.properties.genus_name}`}</div>) : null}
+                  {tree.properties.condition ? (<div>{`Condition: ${tree.properties.condition}`}</div>) : null}
+                  Address: {tree.properties.address}
+                </Popup>
+              </Marker>
+            )
+          })} */}
+      </MarkerClusterGroup>
       </Map>
     );
   }
