ea37de059e8e26c35aad995258c2bbafc998c782
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-28 18:31:33 -0800

    make App into functional comp. + use redux

--- client/src/App.js
+++ client/src/App.js
@@ -8,183 +8,88 @@ import SavedList from "./components/SavedList";
 import SavedListItem from "./components/SavedListItem";
 import TrackList from "./components/TrackList";
 import TrackListItem from "./components/TrackListItem";
-// import Button from "./components/Button";
 
 import { Body, Section, Paragraph, Button, ButtonStyles, RecentPlaylistStyle, PlaylistContainer, PlaylistItem, Map } from './components/style.js';
 
-// import { getAppointmentsForDay } from "./helpers/selectors";
-
-
-// const Button = styled.button`${ButtonStyles};`;
-
-// const Main = () => (
-//   <Section>
-//     <Paragraph>
-//       Look at my buttons, they are amazing buttons !
-//     </Paragraph>
-//     <Button type="primary">
-//       A Primary Button
-//     </Button>
-//     <Button type="secondary">
-//       And I am a secondary
-//     </Button>
-//   </Section>
-// );
 import { connect } from 'react-redux'
 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: "Songs go here!",
-      tracks: [
-        { id: 1, artist: "Enya", title: "Orinoco Flow", albumcover: "https://i.imgur.com/LpaY82x.png" },
-        { id: 2, artist: "Moby", title: "Porcelain", albumcover: "https://i.imgur.com/Nmx0Qxo.png" },
-        { id: 3, artist: "Led Zeppelin", title: "Stairway to Heaven", albumcover: "https://i.imgur.com/T2WwVfS.png" },
-        { id: 4, artist: "Ludovico Einaudi", title: "Divenire", albumcover: "https://i.imgur.com/FK8V841.jpg" },
-        { id: 5, artist: "Crosby Stills & Nash", title: "Cathedral", albumcover: "https://i.imgur.com/twYrpay.jpg" }
-      ]
-    }
-  }
+const App = function (props) {
 
-  fetchData = () => {
+  const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
       .then((response) => {
         // handle success
         console.log(response.data) // The entire response from the Rails API
 
         console.log(response.data.message) // Just the message
-        this.setState({
+        props.dispatch({
+          type: 'UPDATE_MESSAGE',
           message: response.data.message
         });
       })
   }
 
-  render() {
-    return (
+  return (
 
-      <main className="layout">
-        <div className="App">
-          <h1>{this.state.message}</h1>
-          <button onClick={this.fetchData} >
-            Fetch Data
+    <main className="layout">
+      <div className="App">
+        <h1>{props.message}</h1>
+        <button onClick={fetchData} >
+          Fetch Data
         </button>
-        </div>
+      </div>
 
-        <section className="sidebar">
+      <section className="sidebar">
 
-          <hr className="sidebar__separator sidebar--centered" />
-          <nav className="sidebar__menu">
-            <PlaylistContainer recent> <RecentlyPlayedList />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <PlaylistContainer recent> <RecentlyPlayedList />
             <RecentlyPlayedListItem />
-            </PlaylistContainer>
-          </nav>
+          </PlaylistContainer>
+        </nav>
 
 
-        </section>
+      </section>
 
-        {/* <section><Map /><img src="https://rumsey3.s3.amazonaws.com/images/arctic.jpg" /> </section> */}
+      {/* <section><Map /><img src="https://rumsey3.s3.amazonaws.com/images/arctic.jpg" /> </section> */}
 
 
-        <section>
-          <PlaylistContainer nearby>
+      <section>
+        <PlaylistContainer nearby>
           <TrackList />
           <TrackListItem />
-          </PlaylistContainer>
-        </section>
+        </PlaylistContainer>
+      </section>
 
-        <section className="schedule">
-          {/* {this.state.tracks} */}
-          <PlaylistContainer saved> 
+      <section className="schedule">
+        {/* {this.state.tracks} */}
+        <PlaylistContainer saved>
           <PlaylistItem saved> Saved Item - Song 1 </PlaylistItem>
           <PlaylistItem saved> Saved Item - Song 2 </PlaylistItem>
           <PlaylistItem saved> Saved Item - Song 3 </PlaylistItem>
-          </PlaylistContainer>
+        </PlaylistContainer>
 
-          <PlaylistContainer recent > 
+        <PlaylistContainer recent >
           <PlaylistItem recent> Recent List - Song 1 </PlaylistItem>
           <PlaylistItem recent> Recent List - Song 2 </PlaylistItem>
           <PlaylistItem recent> Recent List - Song 3 </PlaylistItem>
-        
-          </PlaylistContainer>
-        </section>
+
+        </PlaylistContainer>
+      </section>
 
 
-        <div>
-          <PlaylistContainer>
+      <div>
+        <PlaylistContainer>
           <Button>I am a Button</Button>
           <Button primary>I am a Primary Button</Button>
           <Button home>Take me home!</Button>
-          </PlaylistContainer>
-        </div>
-        );
-      }
-      </main>
-    );
-  }
-}
-
-
-
-/* REFERENCE CODE FROM SCHEDULER - Application.js
-
-export default function Application(props) {
-  const {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
-  } = useApplicationData();
-
-  const interviewers = getInterviewersForDay(state, state.day);
-  
-  const appointments = getAppointmentsForDay(state, state.day).map(
-    appointment => {
-      return (
-        <Appointment
-          key={appointment.id}
-          {...appointment}
-          time={appointment.time}
-          interview={getInterview(state, appointment.interview)}
-          interviewers={interviewers}
-          bookInterview={bookInterview}
-          cancelInterview={cancelInterview}
-        />
+        </PlaylistContainer>
+      </div>
       );
     }
-  );
-
- 
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList days={state.days} day={state.day} setDay={setDay} />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />
-      </section>
-      <section className="schedule">
-        <section className="schedule">
-          {appointments} 
-          <Appointment key="last" time="5pm" />
-        </section>
-      </section>
-    </main>
+      </main>
   );
 }
-
-*/
-
 
 export default connect(state => state)(App);
