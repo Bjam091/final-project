c47766285154b0046669ebadb4e3a70f7281ffbb
Julia Jachimowicz <juliaj621@gmail.com>
2019-11-28 17:09:26 -0800

    clean  up code

--- client/src/helpers/mapHelpers.js
+++ client/src/helpers/mapHelpers.js
@@ -1,26 +1,19 @@
-// Get user's location
-window.lat = 49.2812;
-window.lng = -123.12345;
-
-function addLocationWatcher(cb) {
+const addLocationWatcher = function (cb) {
   if (!cb) {
-    cb = updatePosition
-  }
+    cb = updatePosition;
+  };
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(cb);
-  }
-
+  };
   return null;
 };
 
-function updatePosition(position) {
+const updatePosition = function (position) {
   if (position) {
     window.lat = position.coords.latitude;
     window.lng = position.coords.longitude;
     console.log("shoop da woop", [window.lat, window.lng]);
-    // document.querySelector('.horseapples').innerHTML = `hmn ${window.lat}, ${window.lng}`;
-    // return [window.lat, window.lng]
-  }
-}
+  };
+};
 
-export { addLocationWatcher}
+export { addLocationWatcher }
