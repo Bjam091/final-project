6ee5732623ae67b5ae9552fb987ba3e0700fef36
Brandon Miller <brandonjamiller@hotmail.com>
2019-12-03 14:34:19 -0800

    add like route started

--- client/src/components/TrackLike/index.js
+++ client/src/components/TrackLike/index.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import api from '../../utils/api';
-// import { likeTrack, getLikes } from './redux';
 
 export class TrackLike extends Component {
   constructor(props) {
