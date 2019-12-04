57b1cec6b85592e3e380111b082a9be68f480496
Anne Franklin <anmerfrank@gmail.com>
2019-12-04 01:14:44 +0000

    Fix merge issue.

--- client/src/components/TrackLike/Like.js
+++ client/src/components/TrackLike/Like.js
@@ -20,7 +20,7 @@ export class Like extends PureComponent {
   }
 
   fetchLikeTracks() {
-    axios.get(`${api.url}/tracks/liked_tracks?user_id=1`)
+    axios.get(`${api.url}/tracks/liked_tracks?user_id=${this.props.auth.user.id}`)
     .then((response) => {
       this.setState({ tracks: response.data });
     });
@@ -34,22 +34,20 @@ export class Like extends PureComponent {
       <div className='trackitemb'>
       
         <div className='flexb'>
-           {/* <div> */}
+          
           <iframe className='current-track-imageb' src={`https://open.spotify.com/embed/track/${track.spotify_uuid}`} width="80" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           <div className='track-textb'>
             <div><strong>{track.title}</strong></div>
             <div>{track.artist}</div>
             <div class="likesb"><button type="button" className='like-buttonb' >
-              {/* onClick={fetchCurrentSong}> */}
-              {/* <form id="form-id" method="post" action="https://listen-in.herokuapp.com/tracks/like/${track.id}"> */}
+       
           <img src="https://i.ibb.co/28TYZtK/heart-filled.png" class="like-buttonb" alt="heart-fill" onclick="document.getElementById('form-id').submit();"/>
-        {/* </form> */}
-              {/* <img src="https://i.ibb.co/Ny59PtM/heart-outline.png" alt="heart-outline" /> */}
+       
             </button># users like this</div> 
             </div>
           </div>
           <div class="spotify-buttonb"> <a href={track.spotify_url}> <img className="spotify-badgeb" src="https://taylorbennett.co/wp-content/uploads/2018/02/spotify-badge-button.png"/></a> </div>
-        {/* </div> */}
+     
       </div>
     </div>
       )
